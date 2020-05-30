d4c7c7bfda3f72c139a1d12efe0d2de589dfaf35
Antonio Yip <antoniokly@users.noreply.github.com>
2020-05-31 11:28:49 +1200

    package platform

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "StravaSwift",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+        .macOS(.v10_10), .iOS(.v8)
     ],
     products: [
         .library(name: "StravaSwift", targets: ["StravaSwift"]),
